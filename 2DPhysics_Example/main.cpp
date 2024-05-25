899f7644972ecfbf1040e7c3e5b65114e0b6bc50
E-Dawkins <ethandawkins@hotmail.co.uk>
2024-05-25 13:49:24 +1000

    Added Raylib to the 2DPhysics_Example project

--- 2DPhysics_Example/main.cpp
+++ 2DPhysics_Example/main.cpp
@@ -1,7 +1,28 @@
-#include <iostream>
+#include "raylib.h"
+
+void Draw();
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    InitWindow(800, 500, "2D Physics Example");
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+
+        Draw();
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+    
     return 0;
+}
+
+void Draw()
+{
+    ClearBackground(RAYWHITE);
+    DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
 }
